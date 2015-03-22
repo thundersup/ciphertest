07bac11b6b084b0614a8e76a7c533a0a981f8ef8
thundersup <ej21044@icloud.com>
2015-03-21 23:26:33 -0400

    Update test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <cmath>
+const int MAX = 65;
+const int MAX_LET = 26;
+ 
+using namespace std;
+
+int toCipher(string &word, string &strcipher, int &message, int &key, int &cipher);
+int toDecipher(string &word1, int &message, int &key1, int &cipher, string &strcipher2);
+int main(){
+  cout << "hello world!" << endl;
+  return 0;
+  }
